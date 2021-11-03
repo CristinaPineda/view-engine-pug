d74983d76a9f3108d836663f846fd765b477762d
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-11-03 17:54:37 -0300

    render pug ok

---
+++ express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => 
+	res.render('index')
+);
+
+app.listen(3000, () => 
+	console.log('Servidor iniciado na porta 3000')
+);
+
